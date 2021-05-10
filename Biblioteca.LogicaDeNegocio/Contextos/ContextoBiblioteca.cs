8b104c39c1d2261b1ab247a9fac9b1554df66249
Daniel Medrano Huerta <daniel@quicktortoise.com>
2021-05-10 17:00:08 -0500

    Agregar nombres de las tablas

--- Biblioteca.LogicaDeNegocio/Contextos/ContextoBiblioteca.cs
+++ Biblioteca.LogicaDeNegocio/Contextos/ContextoBiblioteca.cs
@@ -16,5 +16,18 @@ namespace Biblioteca.LogicaDeNegocio.Contextos
         public DbSet<LibroHasGenero> LibroHasGeneros { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
         public DbSet<Favorito> Favoritos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Autor>(builder => builder.ToTable("Autor"));
+            modelBuilder.Entity<Genero>(builder => builder.ToTable("Genero"));
+            modelBuilder.Entity<Editorial>(builder => builder.ToTable("Editorial"));
+            modelBuilder.Entity<Libro>(builder => builder.ToTable("Libro"));
+            modelBuilder.Entity<LibroHasGenero>(builder => builder.ToTable("LibroHasGenero"));
+            modelBuilder.Entity<Usuario>(builder => builder.ToTable("Usuario"));
+            modelBuilder.Entity<Favorito>(builder => builder.ToTable("Favorito"));
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }

4be5624cd07778344c78083b82f938b977607b63
Daniel Medrano Huerta <daniel@quicktortoise.com>
2021-05-10 16:42:24 -0500

    Agregar más métodos 'get' al repositorio

--- Biblioteca.LogicaDeNegocio/Servicios/IRepositorioBiblioteca.cs
+++ Biblioteca.LogicaDeNegocio/Servicios/IRepositorioBiblioteca.cs
@@ -5,7 +5,48 @@ namespace Biblioteca.LogicaDeNegocio.Servicios
 {
     public interface IRepositorioBiblioteca
     {
+        // Autores
         IEnumerable<Autor> ObtenerAutores();
         Autor ObtenerAutor(int idAutor);
+        // void ActualizarAutor(Autor autor);
+        // void CrearAutor(Autor autor);
+        // void EliminarAutor(Autor autor);
+
+        // Editoriales
+        IEnumerable<Editorial> ObtenerEditoriales();
+        Editorial ObtenerEditorial(int idEditorial);
+        // void ActualizarEditorial(Editorial editorial);
+        // void CrearEditorial(Editorial editorial);
+        // void EliminarEditorial(Editorial editorial);
+
+        // Géneros
+        IEnumerable<Genero> ObtenerGeneros();
+        Genero ObtenerGenero(int idGenero);
+        // void ActualizarGenero(Genero genero);
+        // void CrearGenero(Genero genero);
+        // void EliminarGenero(Genero genero);
+        
+        // Libro
+        // IEnumerable<Libro> ObtenerLibros();
+        // Libro ObtenerLibro(ulong idLibro);
+        // void ActualizarLibro(Libro libro);
+        // void CrearLibro(Libro libro);
+        // void EliminarLibro(Libro libro);
+        
+        // Usuario
+        IEnumerable<Usuario> ObtenerUsuarios();
+        Usuario ObtenerUsuario(string idUsuario);
+        // void ActualizarUsuario(Usuario usuario);
+        // void CrearUsuario(Usuario usuario);
+        // void EliminarUsuario(Usuario usuario);
+        
+        // Favoritos
+        // IEnumerable<Favorito> ObtenerFavoritos();
+        // Favorito ObtenerFavorito(int idFavorito);
+        // void ActualizarFavorito(Favorito favorito);
+        // void CrearFavorito(Favorito favorito);
+        // void EliminarFavorito(Favorito favorito);
+
+        bool Save();
     }
 }
--- Biblioteca.LogicaDeNegocio/Servicios/RepositorioBiblioteca.cs
+++ Biblioteca.LogicaDeNegocio/Servicios/RepositorioBiblioteca.cs
@@ -25,6 +25,41 @@ namespace Biblioteca.LogicaDeNegocio.Servicios
             return _db.Autores.FirstOrDefault(a => a.Id == idAutor);
         }
 
+        public IEnumerable<Editorial> ObtenerEditoriales()
+        {
+            return _db.Editoriales.ToList();
+        }
+
+        public Editorial ObtenerEditorial(int idEditorial)
+        {
+            return _db.Editoriales.FirstOrDefault(e => e.Id == idEditorial);
+        }
+
+        public IEnumerable<Genero> ObtenerGeneros()
+        {
+            return _db.Generos.ToList();
+        }
+
+        public Genero ObtenerGenero(int idGenero)
+        {
+            return _db.Generos.FirstOrDefault(g => g.Id == idGenero);
+        }
+
+        public IEnumerable<Usuario> ObtenerUsuarios()
+        {
+            return _db.Usuarios.ToList();
+        }
+
+        public Usuario ObtenerUsuario(string idUsuario)
+        {
+            return _db.Usuarios.FirstOrDefault(u => u.Id == idUsuario);
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() >= 0;
+        }
+
         public void Dispose()
         {
             Dispose(true);
